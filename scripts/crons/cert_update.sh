322fc331a30a6e0f62b62c98e8f4253f926e654b
Vitalik <v.arykau@arateg.by>
2019-02-13 13:33:45 +0300

    initial commit

---
+++ scripts/crons/cert_update.sh
@@ -0,0 +1,28 @@
+#!/bin/sh
+
+thirty_days=2592000
+export hostnames=$(echo $HOSTNAMES | sed "s/ //g")
+# name of first domain
+
+# Ends with status code 1 if it will expired or 0 if it will not
+if openssl x509 -checkend $thirty_days -noout -in $cert_path; then
+    # make backup, create new cert and copy to cert_path
+    cp -fv /etc/letsencrypt/live/$domain_cert_dir . /etc/nginx/certificates/backup
+    /opt/letsencrypt/letsencrypt-auto certonly --email ${EMAIL} --agree-tos --renew-by-default --non-interactive --webroot -w /usr/share/nginx/html -d $hostnames
+    # If update was good
+    le_result=$?
+    if [ ${le_result} -ne 0 ]; then
+        echo "failed to run certbot"
+        return 1
+    fi
+    cp -fv /etc/letsencrypt/live/$domain_cert_dir . $cert_path
+    # reload nginx with new certs
+    nginx -s reload
+fi
+
+return 0
+
+# BAD usage because server can have other certificates from other applications not related to this project
+#certificates_data=$(/opt/letsencrypt/certbot-auto certificates)
+#days_untile_expired=$(echo $certificates_data | grep -oP '(?<=VALID:\s)\w+')
+# AND I GET HERE NUMBER 67 for example/ If something goes wrong(more then 1 certificate I will receive several numbers)
---
+++ scripts/init.sh
@@ -0,0 +1,60 @@
+#!/bin/sh
+
+git clone https://github.com/letsencrypt/letsencrypt --branch v0.31.0 /opt/letsencrypt
+
+SSL_CERT=/etc/nginx/certificates/fullchain.pem
+SSL_KEY=/etc/nginx/certificates/privkey.pem
+SSL_CHAIN_CERT=/etc/nginx/certificates/chain.pem
+
+# Replace SSL_* for path of files
+sed -i "s/SSL_KEY/${SSL_KEY}/g" /etc/nginx/conf.d/*.conf
+sed -i "s/SSL_CERT/${SSL_CERT}/g" /etc/nginx/conf.d/*.conf
+sed -i "s/SSL_CHAIN_CERT/${SSL_CHAIN_CERT}/g" /etc/nginx/conf.d/*.conf
+
+# Maybe if someone decide to write HOSTNAMES="example.com, www.example.com"
+export hostnames=$(echo $HOSTNAMES | sed "s/ //g")
+# name of first domain
+export domain_cert_dir=$(echo $HOSTNAMES | cut -d"," -f1)
+export cert_path=/etc/nginx/certificates/cert.pem
+
+# If certificate exists, then chack domains
+if [ -f $cert_path ]; then
+    # Looking for domains regarding current certificate
+    current_certs=$(openssl x509 -in ${cert_path} -text -noout | egrep -o 'DNS.*' | sed -e "s/DNS://g" | sed -e "s/,/ /g")
+    # compare domains from cert.pem and ENV HOSTNAMES
+    IFS=$',' # Delimeter
+    sorted_currents_certs=($(sort <<<"${current_certs[*]}"))
+    sorted_hostnames=($(sort <<<"${hostnames[*]}"))
+    unset IFS
+    certs_domains_diff=$(echo ${sorted_currents_certs[@]} ${sorted_hostnames[@]} | tr ' ' '\n' | sort | uniq -u)
+    # IF new names then create new certificate, but make backup it at first. Later copy new cert to cert_dir
+    if [-z "$certs_domains_diff"]; then
+        # Directory could be named by another domain
+        cp -fv /etc/letsencrypt/live/$domain_cert_dir . /etc/nginx/certificates/backup 2>/dev.null
+        /opt/letsencrypt/letsencrypt-auto certonly --email ${EMAIL} --renew-by-default --agree-tos --expand --non-interactive --webroot -w /usr/share/nginx/html -d $hostnames
+        cp -fv /etc/letsencrypt/live/$domain_cert_dir . $cert_path
+    fi
+fi
+
+#If no cert.pem create certificate
+if [! -f $cert_path ]; then
+    opt/letsencrypt/letsencrypt-auto certonly --email ${EMAIL} --agree-tos --non-interactive --webroot -w /usr/share/nginx/html -d $hostnames
+    cp -fv /etc/letsencrypt/live/$domain_cert_dir . $cert_path
+fi
+
+# cp twice. I thought to create COPY independent but  there is a possible
+# situation when file exists, domains are the same but first domain name is different. So path to cert could differ
+
+
+# Commented because last command began "copy"
+# le_result=$?
+# if [ ${le_result} -ne 0 ]; then
+#    echo "failed to run certbot"
+#    return 1
+# fi
+
+#check 30 days before cron
+/opt/letsencrypt/crons/cert_update.sh
+# ========== Script for crontab for updating =======
+# Run monthly
+crontab /opt/letsencrypt/crons/cron_renew
